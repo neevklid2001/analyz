592222cd5a14cf60b4b6e23d1073d514a54b2f1f
neevklid2001 <105793227+neevklid2001@users.noreply.github.com>
2022-11-14 12:19:22 +0300

    Update task5.py

--- task5/task5.py
+++ task5/task5.py
@@ -57,47 +57,9 @@ def task(dataA,dataB):
   tableA=get_table(dataA)
   tableB=get_table(dataB)
   mergedTable=tableA*tableB+tableA.T*tableB.T
-  no_diff=[]
-  for i in range(mergedTable.shape[0]):
-    no_diff.append([])
+  answer=[]
   for j in range(mergedTable.shape[1]):
-    for i in range(mergedTable.shape[0]):
-      if (mergedTable[i][j]==0 or mergedTable[i][j]==2) and i!=j:
-        no_diff[j].append(i)
-  for i in range(len(no_diff)):
-    for item in no_diff[i]:
-      for j in range(len(no_diff)):
-        if item in no_diff[j]:
-          if i not in no_diff[j]:
-            no_diff[j].append(i)
-          if j not in no_diff[i]:
-            no_diff[i].append(j)
-  colorful=[0]*len(no_diff)
-  color=1
-  for i in range(len(colorful)):
-    if colorful[i]==0:
-      if len(no_diff[i])!=0:
-        for item in no_diff[i]:
-          colorful[item]=color
-      else:
-        colorful[i]=color      
-      color+=1
-  diff=[]
-  for i in range(1,color):
-    diff.append(colorful.index(i))
-  tableMult=tableA*tableB
-  table_count=[0]*len(diff)
-  for j in range(len(diff)):
-    for i in range(tableMult.shape[0]):
-      table_count[j]+=tableMult[i,diff[j]]
-  pairs=[]
-  for i in range(len(table_count)):
-    pairs.append([table_count[i],diff[i]])
-  pairs.sort(key = lambda x:x[0], reverse=False)
-  experts=[]
-  for item in pairs:
-    if len(no_diff[item[1]])!=0:
-      experts.append(sorted(no_diff[item[1]]))
-    else:
-      experts.append(item[1])
-  return back_string(experts)
+    for i in range(j):
+      if mergedTable[i,j]==0:
+        answer.append([i,j])
+  return back_string(answer)
